1fca7cd579dd0716aaae815c6a9e94e4cd2cf673
droland2 <droland2@huskers.unl.edu>
2023-04-08 11:09:46 -0500

    Moved `find_best_from_all` to Identifier ABC, since it's yet to be changed.

--- ti4-nlp/identifiers/identifiers.py
+++ ti4-nlp/identifiers/identifiers.py
@@ -13,12 +13,6 @@ class Identifier:
     def find_best(self, token):
         return
 
-    @abc.abstractmethod
-    def find_best_from_all(self, tokens):
-        return
-
-
-class SimpleIdentifier(Identifier):
     def find_best_from_all(self, tokens):
         tokens = [token.lower() for token in tokens]
         all_results = []
@@ -52,6 +46,8 @@ class SimpleIdentifier(Identifier):
             all_results.append((best_token, results))
         return all_results
 
+
+class SimpleIdentifier(Identifier):
     def find_best(self, token):
         best = ([], self.threshold)
         for word, keyword in self.word_index.alias_index.items():
@@ -83,39 +79,6 @@ class GraphPruningIdentifier(Identifier):
                 if distance < self.threshold:
                     self.graph[a][distance].add(b)
                     self.graph[b][distance].add(a)
-
-    def find_best_from_all(self, tokens):
-        tokens = [token.lower() for token in tokens]
-        all_results = []
-        debt = 0
-
-        for index, token in enumerate(tokens):
-            # When we choose to hoard multiple tokens, we use this to skip them
-            # when they should be investigated otherwise. Ends up behaving as a
-            # greedy algorithm, for better and for worse.
-            if debt > 0:
-                debt = debt - 1
-                continue
-
-            best_token = token
-            best = self.find_best(token)
-            i = 1
-            new_token = token
-            contender = ([], 0)  # This is never used but PyCharm seems to care
-            while (i == 1 or len(contender[0])) and index + i < len(tokens):
-                new_token = new_token + ' ' + tokens[index + i]
-                contender = self.find_best(new_token)
-                if contender[1] < best[1]:
-                    best = contender
-                    best_token = new_token
-                    debt = i
-                i = i + 1
-
-            results = []
-            if best[0] is not None:
-                [results.append(item) for item in best[0] if item not in results]
-            all_results.append((best_token, results))
-        return all_results
 
     def find_best(self, token):
         eligible_words = set(self.word_index.alias_index.keys())

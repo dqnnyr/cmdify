df3037410f7bd6b22eb6357bdf4f4f5fb777cb1f
Matt Lobmeyer <mlobmeyer2@huskers.unl.edu>
2023-04-13 16:39:32 -0500

    Added documentation and made code more readable
    - Updated names of things to make them make more sense intuitively
    - Added docstring to _add_to_index
    - Added a bunch of comments. Some are helpful. Some are not.

--- README.md
+++ README.md
@@ -5,17 +5,36 @@ Will be available with Release-0.1.
 ## Usage
 
 ```python
+# Why do we not have a main function with this in it?
 from ti4nlp.lexica import generate_index_and_classifier
 from ti4nlp.identifiers import GraphPruningIdentifier
 from ti4nlp.processors import SimpleQueryProcessor
 from ti4nlp.result import Success, Failure, AmbiguousWordError, UnrecognizedWordError
 
+# This is the vocabulary that is used to classify words
+# It's a dictionary, but why do some keys have blank lists? 
+# Ex: Lightning
+# Would players be added to the data during the game? 
+# Why is it formatted like this? 
+
+# To answer my own questions, this is a list of terms (classified by parts of speech)
+# Each key is a general word, with the values being any synonyms that a user might type instead
+# Example: in this data, the only type of card is a spell. 
+# A player could "play a card" or "play a spell" and both would have the same result
+# this should probably be called "terms" or "vocabulary" because "data" is too vague
 data = {
+    # parts of speech
     'noun': {
+        # general nouns followed by the "types" that relate to each word
+        # the one that really throws me off is spell and card. 
+        # The list format makes it seem like several types of cards could be played
+        # I understand now what the main idea is, but I think more documentation is needed/better comments
+        # Most of this is not "intutive" if you don't already know what's going on
         'card': ['spell'], 'tableau': ['board'], 'hand': [], 'player': [],
         'Fireball': [], 'Lightning': [], 'Bubble Beam': [],
     },
     'qualifier': {
+        # here it makes sense because 'quick' actually is an alias of fast
         'fast': ['quick'], 'slow': [],
         'red': [], 'yellow': [], 'blue': [], 'green': [],
     },
@@ -25,14 +44,25 @@ data = {
     'skip_word': {'a': ['an'], 'the': []},
 }
 
+# See lexica.py (creates the index and the classifier)
+# Also why **data?
+# index is too broad of a term
+# classifier kinda makes sense
 index, classifier = generate_index_and_classifier(**data)
 
+# See identifiers.py (uses the index to remove irrelevant words?)
+# identifier might also be too vague
 identifier = GraphPruningIdentifier(index, threshold=6)
+
+# See processors.pu (processes the query)
+# takes in the classifier and identifier in order to actually process the query
 processor = SimpleQueryProcessor(classifier, identifier)
 
 # The GraphPruningIdentifier utilizes fuzzy detection.
+# Reads query, returns results
 output = processor.process('play fireblal on the blue player and discard Lightning')
 
+# Is success a function? Where does it come from? Why is it being imported instead of set as a value?
 if isinstance(output, Success):
     for action in output.result:
         print(action)
@@ -42,6 +72,7 @@ elif isinstance(output, Failure):
             print(f'Ambiguous word "{error.word}" (could be: {", ".join(error.options)})')
         elif isinstance(error, UnrecognizedWordError):
             print(f'Unrecognized word "{error.word}"')
+# What happens if somehow neither of these cases happen? Why is it set up as an elif instead of else?
 ```
 
 Expected output:
@@ -49,3 +80,4 @@ Expected output:
 {'verb': 'play', 'components': ['Fireball'], 'on': ['blue player']}
 {'verb': 'discard', 'components': ['Lightning']}
 ```
+So what do we do with the output?
--- ti4nlp/lexica.py
+++ ti4nlp/lexica.py
@@ -1,3 +1,4 @@
+# So I kept both because idk what changes you made and I didn't wanna fuck with shit
 class WordIndex:
     def __init__(self, **words_and_aliases: list[str]):
         self._alias_index = {}
@@ -39,6 +40,57 @@ class WordClassifier:
     def __getattr__(self, attr: str) -> set[str]:
         return self._data[attr]
 
+### END OF YOUR STUFF BEGINNING OF MINE ###
+# tastes really good, but a little dry, could use some more comments
+
+class SynonymIndex:
+    def __init__(self, **all_words):
+        self.synonym_index = {}
+        for primary_term, synonyms in all_words.items():
+            self._add_to_index(primary_term, synonyms)
+
+    def _add_to_index(self, primary_term: str, synonyms: list[str]):
+        #"what this function does" --> redudant and needs to be removed
+        """
+        Ex: {'work': ['act', 'do', 'labor']} 
+        primary_term = 'work'
+        synonyms = ['act', 'do', 'labor']
+        This basically reverses the dictionary. 
+        The keys are 'act', 'do', and 'labor', all with the value 'work'. 
+        If the query has any synonyms, they all "translate" to the same word: 'work'. 
+        """
+        lowercase = primary_term.lower()
+        # if the term is already a key in the dictionary, keyword = value of term
+        if lowercase in self.synonym_index:
+            value = self.synonym_index[lowercase]
+        else:
+            # perserves casing of the "original" word
+            value = primary_term
+            # add term to dict using lowercase as key
+            # Ex: 'tomato': 'Tomato'
+            self.synonym_index[lowercase] = value
+        for alias in synonyms:
+            # for each synonym, add the keyword as the value
+            # Ex: 'ketchup' : 'Tomato', 'marinara': 'Tomato'
+            # Would be formatted in data as 'Tomato' : ['ketchup', 'marinara']
+            self.synonym_index[alias.lower()] = value
+
+
+class WordClassifier:
+    def __init__(self, **classes_and_words):
+        self.data = {}
+        # the fuck is a clazz. Daniel. What is this name Daniel. 
+        # Daniel. I googled it. This is a Java convention. Daniel. This is python. The file is named lexia.py. 
+        # What are you doing Daniel. 
+        for clazz, words in classes_and_words.items():
+            # what is a "class" of word? 
+            # part of speech = keys of part of speech
+            self.data[clazz] = set(words)
+
+    # get 
+    def __getattr__(self, attr):
+        return self.data[attr]
+
     def __getitem__(self, item: str) -> set[str]:
         return self._data[item]
 
@@ -56,14 +108,24 @@ class WordClassifier:
 
     def values(self):
         return self._data.values()
-
-
+# I did keep this one line from your stuff though
+# I got rid of the other one
 def generate_index_and_classifier(**class_words_and_aliases: dict[str, list[str]]) -> tuple[WordIndex, WordClassifier]:
+    # The rest of this should be my stuff
+    # makes a dictionary with k = part of speech, and then the keys are made into a list
     classifications = {k: list(v.keys()) for k, v in class_words_and_aliases.items()}
     term_aliases = {}
+    # class_words_aliases = {greeting: {hi: [hello], sup: []}, farewell: {goodbye:[]}}
+    # alias_dict = {hi: [hello]} (for the first iteration)
+    # Combine all parts of speech dictionaries
     for alias_dict in class_words_and_aliases.values():
+        # term = hi
+        # alias = hello
         for term, aliases in alias_dict.items():
+            # term_aliases = {hi: [hello]}
             term_aliases[term] = aliases
+    # maps synonyms to true word
     index = WordIndex(**term_aliases)
+    # identifies parts of speech
     classifier = WordClassifier(**classifications)
     return index, classifier

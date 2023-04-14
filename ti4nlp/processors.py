29b41ccb771c875c1e66d7a9bc2f6c879f4e1a98
droland2 <droland2@huskers.unl.edu>
2023-04-14 16:35:19 -0500

    Moved `Action` and related classes to `result.py`.

--- ti4nlp/processors.py
+++ ti4nlp/processors.py
@@ -1,73 +1,9 @@
 import abc
 import re
 
-from ti4nlp.lexica import WordClassifier
-from ti4nlp.result import Result, Success, Failure, UnrecognizedWordError, AmbiguousWordError, UnclassifiedWordError
 from ti4nlp.identifiers import Identifier
-
-
-class NounPhrase:
-    def __init__(self, noun: str, qualifiers=None):
-        self._noun = noun
-        self._prepositions = {}
-        if qualifiers is None:
-            self._qualifiers = []
-        else:
-            self._qualifiers = qualifiers
-
-    @property
-    def noun(self):
-        return self._noun
-
-    def __contains__(self, item):
-        return item in self._prepositions
-
-    def add_qualifier(self, qualifier: str):
-        self._qualifiers.append(qualifier)
-
-    def add_preposition(self, preposition: str, dependents):
-        self._prepositions[preposition] = dependents
-
-    def get_prepositions(self):
-        return self._prepositions.items()
-
-
-class PrepositionalPhrase:
-    def __init__(self, preposition: str, dependents: list[NounPhrase]):
-        self.preposition = preposition
-        self.dependents = dependents
-
-
-class Action:
-    def __init__(self):
-        self.verb = None
-        self.direct_objects = []
-        self.prepositional_phrases = []
-        self._last_modified = []
-
-    def has_verb(self) -> bool:
-        return self.verb is not None
-
-    def set_verb(self, verb: str):
-        self.verb = verb
-
-    def add_direct_object(self, direct_object: NounPhrase):
-        self.direct_objects.append(direct_object)
-
-    def add_prepositional_phrase(self, prepositional_phrase: PrepositionalPhrase):
-        cached = self._last_modified
-        self._last_modified = []
-        for noun in self.direct_objects:
-            if prepositional_phrase.preposition not in noun:
-                self._last_modified.append(noun)
-                noun.add_preposition(prepositional_phrase.preposition, prepositional_phrase.dependents)
-        if len(self._last_modified) == 0:
-            for noun in cached:
-                x = noun.copy()
-                self.add_direct_object(x)
-                x.add_preposition(prepositional_phrase.preposition, prepositional_phrase.dependents)
-                self._last_modified.append(noun)
-            self._last_modified = cached
+from ti4nlp.lexica import WordClassifier
+from ti4nlp.result import *
 
 
 class QueryProcessor:

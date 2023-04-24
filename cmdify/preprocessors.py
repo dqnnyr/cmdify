2447e4ba409695b205053b3282e121fed8321b6e
Matt Lobmeyer <mlobmeyer2@huskers.unl.edu>
2023-04-24 15:10:58 -0500

    Added comments and questions (mostly questions)
    Line 19 should probably be broken up for ease of reading

--- cmdify/preprocessors.py
+++ cmdify/preprocessors.py
@@ -1,7 +1,7 @@
 import abc
 import re
 
-
+# What is the point of this. Why is it here. Does it exist solely to have children?
 class Preprocessor(abc.ABC):
     def __init__(self):
         pass
@@ -10,8 +10,14 @@ class Preprocessor(abc.ABC):
     def preprocess(self, query: str) -> list[str]:
         return []
 
-
+# Why do we have a class for this when it could just be one single function? 
 class SimplePreprocessor(Preprocessor):
     def preprocess(self, query: str) -> list[str]:
+        # Creates a list of tokens 
+        # Said tokens have any instance of ',', ';', and '/' replaced with 'and'
+        # Then splits a string into individual tokens based on individual words or phrases (signified with "")
         tokens: list[str] = re.findall(r'[^"\s]\S*|".+?"', re.sub(r'[,;/]+', ' and ', query))
+        # Removes quotes from tokens
         return [token[1:-1].strip() if token[0] == '"' and token[0] == token[-1] else token for token in tokens]
+    
+    # If we need to keep this as a class, why not also add in removing stop words as another function? 
